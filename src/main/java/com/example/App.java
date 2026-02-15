6b04270cc1d8fb1d553e07dc70f043ecabffb1c3
joshvajustin14 <joshvajustin14@gmail.com>
2026-02-15 12:59:30 +0530

    Add main class for Java Jenkins demo project

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("=================================");
+        System.out.println(" Java Jenkins Demo Project ");
+        System.out.println(" Build Successful 🚀 ");
+        System.out.println("=================================");
+    }
+}
